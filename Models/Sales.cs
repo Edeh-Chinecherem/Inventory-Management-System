67d2058274f11bda6632fd620cd795787b03d9f5
Chinecherem <edehchinecherem08@gmail.com>
2025-04-24 11:22:15 +0100

    Added Validation to Sales Model

--- Models/Sales.cs
+++ Models/Sales.cs
@@ -4,8 +4,14 @@ public class Sale
 {
     public int Id { get; set; }
     public DateTime Date { get; set; } = DateTime.Now;
+
+    
+    [Required]
     public int ProductId { get; set; }
     public Product Product { get; set; } = null!;
+
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0")]
     public int QuantitySold { get; set; }
     public decimal TotalPrice { get; set; }
 }
