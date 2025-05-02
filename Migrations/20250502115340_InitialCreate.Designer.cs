c17f7d0a1db66526c2c752b24515bb3e9bc1289c
Chinecherem <edehchinecherem08@gmail.com>
2025-05-02 12:58:56 +0100

    Migration Added

--- Migrations/20250502115340_InitialCreate.Designer.cs
+++ Migrations/20250502115340_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InventoryManagementSystem.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250424105932_InitialCreate")]
+    [Migration("20250502115340_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
